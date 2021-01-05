6a5477d0736f3a4bf30392746c0dff1b5606b6c5
linux <rubykhanhas@gmail.com>
2021-01-05 19:46:18 +0700

    clearchannel handle error

--- src/commands/clearchannel.js
+++ src/commands/clearchannel.js
@@ -1,10 +1,14 @@
 module.exports = {
     name: 'clearchannel',
     description: 'clear chat recent on channel',
-    exec(msg, ...args){
-        msg.channel.messages.fetch().then(result => {
-            msg.channel.bulkDelete(result);
-            msg.channel.send("Cleared!");
-        });
+    async exec(msg, ...args){
+        try {
+            msg.channel.messages.fetch().then(result => {
+                msg.channel.bulkDelete(result);
+                msg.channel.send("Cleared!");
+            });
+        } catch (err) {
+            msg.channel.send(`Clear channel error: ${err}`);
+        }
     }
 }
